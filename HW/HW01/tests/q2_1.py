cc43a7ef804f88100dc3a376f890594753e4d68c
b00chan <103214755+b00chan@users.noreply.github.com>
2024-01-22 11:20:48 -0800

    Add files via upload

---
+++ HW/HW01/tests/q2_1.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q2_1',
+    'points': [0, 4],
+    'suites': [   {   'cases': [{'code': '>>> 1 <= characters_q1 <= 5\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> characters_q1 == 2\nTrue', 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ HW/HW01/tests/q4_1.py
@@ -0,0 +1,10 @@
+OK_FORMAT = True
+
+test = {   'name': 'q4_1',
+    'points': [0, 5],
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(biggest_change, (int, float))\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> biggest_change == 57\nTrue', 'hidden': True, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
